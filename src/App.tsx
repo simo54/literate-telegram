cc51321236cdd9613996d9d6707e6806394d5dd7
simone <sorzesimone1@gmail.com>
2021-04-10 22:05:35 +0200

    testing default props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { Heading } from "./components/Heading";
+import { Heading, HeadingWithContent, Container } from "./components/Heading";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Heading title="Hey there"></Heading>
+      <HeadingWithContent>
+        <strong>Hello</strong>
+      </HeadingWithContent>
+      <Container>another hey</Container>
     </div>
   );
 }
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,4 +1,35 @@
+import { ReactElement, ReactNode } from "react";
+
 // Conventional props
 export function Heading({ title }: { title: string }) {
   return <h1>{title}</h1>;
 }
+
+export function HeadingWithContent({
+  children,
+}: {
+  children: ReactNode;
+}): ReactElement | null {
+  return <h2>{children}</h2>;
+}
+
+// Default props
+const defaultContainerProps = {
+  heading: <strong>My Heading with strong tag</strong>,
+};
+
+export function Container({
+  heading,
+  children,
+}: {
+  children: ReactNode;
+} & typeof defaultContainerProps): ReactElement {
+  return (
+    <div>
+      <h3>{heading}</h3>
+      {children}
+    </div>
+  );
+}
+
+Container.defaultProps = defaultContainerProps;
